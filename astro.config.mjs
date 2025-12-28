5231994d9d7c463f9c638029586a5d57719bc526
nvconfig-sketch <nvconfig@gmail.com>
2025-12-28 16:13:39 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,21 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'DM-Safety',
-			locales: {
-   			 he: {
-    			  label: 'עברית',
-    			  lang: 'he',
-     			  dir: 'rtl',
-				  root: true,
-				    },
-  },
-  defaultLocale: 'he',
-})
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: 'DM-Safety',
+      locales: {
+        he: {
+          label: 'עברית',
+          lang: 'he',
+          dir: 'rtl',
+          root: true,
+        },
+      },
+      defaultLocale: 'he',
+      customCss: ['src/styles/rtl.css'],
+      // פה שאר ההגדרות שלך: social, sidebar וכו'
+    }),
+  ],
 });
